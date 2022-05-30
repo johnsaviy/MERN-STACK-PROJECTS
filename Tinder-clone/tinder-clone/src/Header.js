d4d8d6cf59ebe09cf9a49910cf43a930501abfec
John Saviour <johnsaviour88@gmail.com>
2022-05-30 09:45:34 +0100

    created the header

--- Tinder-clone/tinder-clone/src/Header.js
+++ Tinder-clone/tinder-clone/src/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import "./Header.css";
-
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
 function Header() {
   return (
     <div className="header">
-        <h2>I am a header</h2>
+        <IconButton>
+           <PersonIcon fontSize="large" className="header_icon" />
+        </IconButton>
+       
+
+       <IconButton>
+          <img 
+       className="header_logo"
+       src="https://p.kindpng.com/picc/s/19-195266_tinder-icon-2017-logo-vector-gradient-tinder-logo.png"
+
+       alt=""
+       />
+
+       </IconButton>
+       
+      <IconButton>
+           <ForumIcon fontSize="large" className="header_icon" />
+        </IconButton>
+       
 
     </div>
   )
