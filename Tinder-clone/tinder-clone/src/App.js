130f51094e412099c73bf43cfac6d1337eeefb2d
John Saviour <johnsaviour88@gmail.com>
2022-05-30 08:20:51 +0100

    added react files

---
+++ Tinder-clone/tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Tinder-clone/tinder-clone/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+        <h2>I am a header</h2>
+
+    </div>
+  )
+}
+
+export default Header
